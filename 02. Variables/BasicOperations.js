fafe91f07b72f30aba94ab0ed0822c3658e17a62
myzty651 <dhanushkac085@gmail.com>
2025-03-19 21:32:14 +0530

    Basic Operations to Variables

---
+++ 02. Variables/BasicOperations.js
@@ -0,0 +1,7 @@
+let a = 10;
+let b = 5;
+
+console.log("Addistion: " + (a + b)); // Addition
+console.log("Subtraction: " + (a - b)); // Subtraction 
+console.log("Multiplication: " + (a * b)); // Multiplication
+console.log("Division: " + (a / b)); // Division
