f195df9fa22386d7536d77d61e401a0c01becd72
myzty651 <dhanushkac085@gmail.com>
2025-03-19 21:43:22 +0530

    Array Operators

---
+++ Array Operators/operations.js
@@ -0,0 +1,13 @@
+//Array operations
+
+//Push and Pop
+ array_1 =['a','b','c'];
+ console.log(array_1);
+ array_1.push('e');
+ console.log(array_1);
+ array_1.pop();
+ console.log(array_1);
+ 
+ //reverse the array using psush and pop
+ //abc => cba
+ 
