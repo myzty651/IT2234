6d2a8d4c409cee2bee6f66ca1e3bff26ec2c1d50
myzty651 <dhanushkac085@gmail.com>
2025-03-19 21:44:07 +0530

    Basic Arrays Creations

---
+++ Basic/array.js
@@ -0,0 +1,40 @@
+let array_1=[1,2,3];
+	for(let i = 0 ; i < array_1.length ; i ++){
+	console.log(array_1[i]);
+	}
+	console.log('\n');
+	console.log(array_1);
+	console.log('\n');
+	array_1.forEach((n)=>{
+	console.log(n);
+});
+	console.log('\n');
+
+	
+let array_2=['Yasas','Lasitha','Aththanayaks'];
+	for(let i = 0 ; i < array_2.length ; i ++){
+	console.log(array_2[i]);
+	}
+	console.log('\n');
+	console.log(array_2);
+	console.log('\n');
+	array_2.forEach((n)=>{
+	console.log(n);
+});
+	console.log('\n');
+
+	
+let array_3=['Yasas',12,'I'];
+	for(let i = 0 ; i < array_3.length ; i++){
+		console.log(array_3[i]);
+	}
+	console.log('\n');
+	console.log(array_3);
+	console.log('\n');
+	array_3.forEach((n)=>{
+	console.log(n);
+});
+	console.log('\n');
+
+
+
