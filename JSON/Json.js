ad9a5e657333f932d0858e7a73b336b5374d77c6
myzty651 <dhanushkac085@gmail.com>
2025-03-19 21:45:27 +0530

    JSON file

---
+++ JSON/Json.js
@@ -0,0 +1,14 @@
+//Json files
+//{key : value}
+ 
+ let student ={regNum : '2021/ict/14', Name : 'Yasas', Age :21 ,
+ course : 'IT', skilled : ['Java','JS','C++']
+ };
+ console.log(student);
+ //console.log(student.name);
+ let students =[
+	 {regNo:'2021/ict/01',Name :'yas', Age:21 ,Course :'IT'},
+	 {regNo:'2021/ict/02',Name :'Las', Age:23 ,Course :'IT'},
+	 {regNo:'2021/ict/03',Name :'Att', Age:21 ,Course :'IT'}
+ ];
+ console.log(students);
